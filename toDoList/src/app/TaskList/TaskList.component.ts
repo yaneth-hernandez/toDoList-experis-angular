35167f4bf95946ebe9cefa9464139267cbe91d16
yaneth-hernandez <yahecaceres@gmail.com>
2023-12-20 08:06:08 -0500

    first commit

---
+++ toDoList/src/app/TaskList/TaskList.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-TaskList',
+  templateUrl: './TaskList.component.html',
+  styleUrls: ['./TaskList.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
